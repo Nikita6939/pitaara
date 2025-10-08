ceedf02ce49ba1c1c4f85a5b9193d47e8f495d19
Nikita6939 <146519119+Nikita6939@users.noreply.github.com>
2025-10-08 11:30:23 +0530

    Update pitaaraCtrl.java

--- src/main/java/com/example/demo/ctrl/pitaaraCtrl.java
+++ src/main/java/com/example/demo/ctrl/pitaaraCtrl.java
@@ -120,7 +120,7 @@ public class pitaaraCtrl {
 
         // Upload gallery photos
         a.setMorePhoto(uploadMultipleToCloudinary(moreFiles));
-       Systemout.println(a.geteference());
+       System.out.println(a.getReference());
         aR.save(a);
         return "redirect:admindashboard";
     }
